6a6c0c4259ea45c64cb7f0dce7f3bcfb3b643ce2
Edwin Taquichiri <edwin.taquichiri@jalasoft.com>
2018-12-19 20:48:55 -0400

    updated login methods and pedido

---
+++ www/js/discal.js
@@ -0,0 +1,11 @@
+var baseUrl = 'http://discald.com/provider/'
+//var baseUrl = 'http://provider.local/'
+
+function getCurrentDate() {
+    var d = new Date();
+       var dateString = d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+       ("0" + d.getDate()).slice(-2) + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) +
+       ":" + ("0" + d.getSeconds()).slice(-2);
+
+    return dateString;
+}
