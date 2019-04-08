d8ea1056f2b8eaf4ab7dbd691f28807b91c58c9f
Edwin Taquichiri <edwin.taquichiri@jalasoft.com>
2019-04-07 22:45:12 -0400

    chnaged to webSQL

--- www/js/index.js
+++ www/js/index.js
@@ -26,8 +26,43 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+    onDeviceReady: function() {        
+        
+        var db = window.sqlitePlugin.openDatabase("Database", "1.0", "demo", -1);
+
+        db.transaction(function(tx) {
+
+            //create table
+            tx.executeSql("CREATE TABLE IF NOT EXISTS demo (id integer primary key, data text, data_num integer)", [], function(tx, res){
+
+                //insert data
+                tx.executeSql("INSERT INTO demo (id, data, data_num) VALUES (?,?,?)", [1, "test", 100], function(tx,res){
+
+                    //retrieve data
+                    tx.executeSql("SELECT * FROM demo WHERE id = ?", [1], function(tx, res){
+                        for(var iii = 0; iii < res.rows.length; iii++)
+                        {
+                            alert(res.rows.item(iii).id);
+                            alert(res.rows.item(iii).data);
+                            alert(res.rows.item(iii).data_num);
+                            console.log(res.rows.item(iii).id);
+                            console.log(res.rows.item(iii).data);
+                            console.log(res.rows.item(iii).data_num);
+                        }
+                        console.log("device ready");
+                        this.receivedEvent('deviceready');
+                    })
+                    
+                });
+
+            });
+
+        }, function(err){
+
+            //errors for all transactions are reported here
+            alert("Error: " + err.message)
+
+        });
     },
 
     // Update DOM on a Received Event
